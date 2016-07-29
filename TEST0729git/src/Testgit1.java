e6cba291ca6cdec8d49e3949793cb668491476eb
sailing <sailing@sailing-PC>
2016-07-29 15:14:58 +0900

    表示変更

--- TEST0729git/src/Testgit1.java
+++ TEST0729git/src/Testgit1.java
@@ -2,6 +2,7 @@
 public class Testgit1 {
 
 	public static void main(String[] args) {
+		System.out.print("‚±‚ê‚Í‹ã‹ã‚Å‚·\n");
 		for (int i=1;i<10;i++){
 			for (int j=1;j<10;j++){
 		       System.out.print(i*j);
